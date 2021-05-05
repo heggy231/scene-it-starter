9315f82d7533fbc164d65840633ef42ea40bc2ef
Heggy Castaneda <heggyy@gmail.com>
2021-05-04 21:40:04 -0400

    fix fall img issue

--- script-new.js
+++ script-new.js
@@ -29,23 +29,17 @@ const renderCard = data => {
 
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
   // https://medium.com/javascript-scene/nested-ternaries-are-great-361bddd0f340
-  const someArr = data.map(item => {
-    const card = document.createElement('div');
-    const img = document.createElement('img'); // <=== object not a string
+  const someArr = data.map((item, index) => {
+    const card = document.createElement('div'); // <div >
+    const img = document.createElement('img'); // <img src="imgurl" clss> <=== object not a string
     img.src = item.imageURL;
     img.setAttribute('class', 'card-img-top');
-    img.onerror = () => {
-      // img.src = './no_image.png';
-      const imagNow = document.getElementById("image-now");
-      imagNow.appendChild(img.src);
-    }
     console.log('img: ', img);
     card.innerHTML = `
       <div class="movie">
           <div class="card" style="width: 18rem;">
-            <div id="image-now">
+            <div id="image-now-${index}">
             </div>
-
             <div class="card-body">
                 <span class="badge badge-secondary">${item.date}</span>
                 <h5 class="card-title">${item.title}</h5>
@@ -54,22 +48,17 @@ const renderCard = data => {
           </div>
       </div>
     `;
-    console.log('card: ', card);
-    window.addEventListener("error", (e) => {
-      console.log('e: ', e);
-      // if(e.type === "error" && e.target === img){
-  
-      // }
-    }, true)
     cards.appendChild(card);
+    const imagNow = document.getElementById(`image-now-${index}`);
+    img.onerror = () => {
+      img.src = './no_image.png';
+      imagNow.appendChild(img);
+    }
+    img.onload = () => {
+      imagNow.appendChild(img);
+    }
+    console.log('card: ', card);
   });
-  
-  window.addEventListener("error", (e) => {
-    console.log('e:', e);
-    // if(e.type === "error" && e.target === img){
-
-    // }
-  }, true)
 };
 
 const button = document.getElementById("button");
